98cb78f50b0701b0cfdf3c899022641498b57392
la9ran9e.laptop <tvauritimur@gmail.com>
2017-03-25 03:43:02 +0300

    исправлен баг ошибки соединения, теперь загрузка возобновляется при восстановлении соединения

--- get_all_hrefs.py
+++ get_all_hrefs.py
@@ -1,46 +1,54 @@
-import requests, re
+import requests
+import re
 from bs4 import BeautifulSoup
 
 hrefs = []
 href_log = []
-p = re.compile(r'^http')
 num = 0
 
-def find(url, SITE):
-    global href_log
+def req(url): # на случай если произойдет разрыв сети или иной сбой подключения
+    try:
+        html = requests.get(url).text
+        return html
+    except (socket.gaierror, requests.exceptions.ConnectionError):
+        req(url)
+
+def rel_orient(href): # преобразование относительных ссылок в жесткие
+    p = re.compile(r'^http')
+    if not p.search(href):
+        href_ = href.split('/')
+        #print(href)
+        if href_[0] == '.':
+            href = '%s/%s' % ('/'.join(url_[:-1]), '/'.join(href_[1:]))
+        elif href_[0] == '..':
+            href = '%s/%s' % ('/'.join(url_[:-2]), '/'.join(href_[1:]))
+        elif href_[0] == '':
+            href = '%s/%s' % ('/'.join(SITE_[:3]), '/'.join(href_[1:]))
+        else:
+            if '.' in url_[-1]:
+                href = '%s/%s' % ('/'.join(url_[:-1]), '/'.join(href_))
+            else:
+                href = '%s/%s' % ('/'.join(url_), '/'.join(href_))
+    return href
+
+def find(url): # нахождение всех ссылок на странице
+    global href_log, url_
     hrefs = []
     if url[-1] == '/':
         url = url[:-1]
     url_ = url.split('/')
-    try:
-        html = requests.get(url).text
-        soup = BeautifulSoup(html, 'html.parser')
-        elems = soup.find_all('a')
-        for elem in elems:
-            href = elem.get('href')
-            if href:
-                if '#' in href:
-                    href = href[:href.index('#')]
-                
-                if not p.search(href):
-                    href = href.split('/')
-                    #print(href)
-                    if href[0] == '.':
-                        href = '%s/%s' % ('/'.join(url_[:-1]), '/'.join(href[1:]))
-                    elif href[0] == '..':
-                        href = '%s/%s' % ('/'.join(url_[:-2]), '/'.join(href[1:]))
-                    elif href[0] == '':
-                        href = '%s/%s' % ('/'.join(SITE_[:3]), '/'.join(href[1:]))
-                    else:
-                        if '.' in url_[-1]:
-                            href = '%s/%s' % ('/'.join(url_[:-1]), '/'.join(href))
-                        else:
-                            href = '%s/%s' % ('/'.join(url_), '/'.join(href))
-                if href not in href_log:
-                    href_log += [href]
-                    hrefs += [href]
-    except (socket.gaierror, requests.exceptions.ConnectionError):
-        pass
+    html = req(url)
+    soup = BeautifulSoup(html, 'html.parser')
+    elems = soup.find_all('a')
+    for elem in elems:
+        href = elem.get('href')
+        if href:
+            if '#' in href:
+                href = href[:href.index('#')]
+            href = rel_orient(href)
+            if href not in href_log:
+                href_log += [href]
+                hrefs += [href]
     return hrefs     
 
 def insert(tree, Childs):
@@ -55,7 +63,7 @@ def expand_(tree, graph, SITE):
             print('<%s> %s' % (num, root(tree)))
         num += 1
         for subtree in subtrees(tree):
-            insert(tree, find(root(tree), SITE))
+            insert(tree, find(root(tree)))
         for subtree in subtrees(tree):
             if root(subtree):
                 if '.' in SITE_[-1]:
